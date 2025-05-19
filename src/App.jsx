9e7f2f58776c92b35d91ada3db8fbb6a7185e9c9
akashpande1886 <72142288+akashpande1886@users.noreply.github.com>
2025-05-19 09:33:04 +0530

    currency convertor added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CurrencyConvertor from "./CurrencyConvertor";
+
+function App() {
+  return (
+    <>
+      <CurrencyConvertor />
+    </>
+  );
+}
+
+export default App;
